0f1bf887f44a6c19b8ffe7ed421e925371b45e94
ahmetacr61 <61ahmetacr61@gmail.com>
2023-09-08 19:24:21 +0200

    addded searching for the programm.java class

--- Wiederholung/programm.java
+++ Wiederholung/programm.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 
 public class programm {
     public static void main(String[] args) {
-        String raumbezeichnung;
+        String raumbezeichnung, raumSuchen;
         int schuelerarbeitsplaetze;
-        boolean besitztWaschbecken;
+        boolean besitztWaschbecken, suchen;
         ArrayList<Raum> raumList = new ArrayList<Raum>();
         Scanner s = new Scanner(System.in);
-        boolean exitCode;
+        boolean exitCode, wurdeRaumGefunden = false;
 
         while(true) {
             System.out.println("------- Geben sie die Daten eines Raumes ein! -------");
@@ -20,6 +20,8 @@ public class programm {
             besitztWaschbecken = s.nextBoolean();
             System.out.println("Eingabe beenden? Geben sie true oder false ein: ");
             exitCode = s.nextBoolean();
+            System.out.println("Möchten sie nach einem Raum suchen? Geben sie true oder false ein");
+            suchen = s.nextBoolean();
             Raum raum = new Raum(raumbezeichnung, schuelerarbeitsplaetze, besitztWaschbecken);
             raumList.add(raum);
             if(exitCode) {
@@ -33,6 +35,24 @@ public class programm {
                 break;
             } else {
 
+            }
+            if(suchen) {
+                System.out.println("Geben sie die Raumbezeichnung ein, nach der gesucht werden soll: ");
+                raumSuchen = s.next();
+                for(int i = 0; i < raumList.toArray().length; i++) {
+                    if(raumList.get(i).getRaumbezeichnung().equals(raumSuchen)) {
+                        System.out.println("Der Raum " + raumList.get(i).getRaumbezeichnung() + " ist vorhanden und hier sind weitere Daten zum Raum: ");
+                        System.out.println("Bezeichnung des Raums: " + raumList.get(i).getRaumbezeichnung());
+                        System.out.println("Anzahl Plaetze: " + raumList.get(i).getSchuelerarbeitsplaetze());
+                        System.out.println("Waschbecken vorhanden: " + raumList.get(i).getBesitztWaschbecken());
+                        wurdeRaumGefunden = true;
+                    }
+                }
+                if(!wurdeRaumGefunden) {
+                    System.out.println("Der Raum " + raumSuchen + " ist nicht im System eingetragen!");
+                } else {
+                    wurdeRaumGefunden = false;
+                }
             }
         }
     }
